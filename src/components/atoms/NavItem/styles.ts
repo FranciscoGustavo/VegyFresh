f0c2e18763318c8b4127546dc12c3f10a3af1e8f
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-08-26 16:00:56 -0500

    feature: component atoms/NanItem

---
+++ src/components/atoms/NavItem/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const CSSNavItem = styled.a`
+display: inline-block;
+width: 35px;
+height: 35px;
+`;
--- src/components/molecules/Nav/index.tsx
+++ src/components/molecules/Nav/index.tsx
@@ -1,10 +1,19 @@
+import { RiBarChart2Line, RiHome7Line } from 'react-icons/ri';
+
 import Avatar from '@/atoms/Avatar';
+import NavItem from '@/atoms/NavItem';
 
 import { CSSNav } from './styles';
 
 const Nav = () => (
   <CSSNav>
     <Avatar />
+    <NavItem to="/">
+      <RiHome7Line />
+    </NavItem>
+    <NavItem to="/all">
+      <RiBarChart2Line />
+    </NavItem>
   </CSSNav>
 );
 
