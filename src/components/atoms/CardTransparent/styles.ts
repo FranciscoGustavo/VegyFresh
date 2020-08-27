a7eb141e0dff4bbab08fa84f6cf1bdd19b39e60c
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-08-26 19:26:26 -0500

    improve: LoginInputs

--- src/components/atoms/CardTransparent/styles.ts
+++ src/components/atoms/CardTransparent/styles.ts
@@ -6,4 +6,8 @@ padding: 28px 14px;
 border-radius: 14px;
 background-color: rgba(255, 255, 255, 0.3);
 box-shadow: 0 0 14px rgba(0,0,0,0.3);
+
+div {
+  margin-bottom: 28px;
+}
 `; 
--- src/components/molecules/LoginInputs/index.tsx
+++ src/components/molecules/LoginInputs/index.tsx
@@ -1,8 +1,22 @@
+import { RiLockPasswordLine, RiUser3Fill } from 'react-icons/ri';
+
 import CardTransparent from '@/atoms/CardTransparent';
+import InputText from '@/atoms/InputText';
 
 const LoginInputs = () => (
   <CardTransparent>
-    HOLA
+    <InputText 
+      type="text"
+      name="email"
+      placeholder="Correo electronico"
+      label={<RiUser3Fill />}
+    />
+    <InputText 
+      type="password"
+      name="password"
+      placeholder="Contraseña"
+      label={<RiLockPasswordLine />}
+    />
   </CardTransparent>
 );
 
